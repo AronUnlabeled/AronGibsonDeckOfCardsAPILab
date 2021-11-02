5d4c0202271a90c38f5a40fb74f2de982b1e8b7a
aronunlabeled <xaron97x@hotmail.com>
2021-11-02 17:44:12 -0400

    first

---
+++ AronGibsonDeckOfCardsAPILab/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using AronGibsonDeckOfCardsAPILab.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AronGibsonDeckOfCardsAPILab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private DeckDAL deckDAL = new DeckDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            DeckModel result = deckDAL.GetData();
+            return View(result);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Reshuffle()
+        {
+            DeckModel result = deckDAL.ShuffleCards();
+            return View(result);
+        }
+
+        public IActionResult DeckResult()
+        {
+            DeckModel result = deckDAL.GetData();
+            return View(result);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AronGibsonDeckOfCardsAPILab/Models/DeckDAL.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace AronGibsonDeckOfCardsAPILab.Models
+{
+    public class DeckDAL
+    {
+        public DeckModel GetData() {
+
+            string url = "https://deckofcardsapi.com/api/deck/0tur1m4uetwf/draw/?count=5";
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+            DeckModel deck = JsonConvert.DeserializeObject<DeckModel>(JSON);
+            return deck;
+        }
+
+        public DeckModel ShuffleCards()
+        {
+
+            string url = "https://deckofcardsapi.com/api/deck/0tur1m4uetwf/shuffle/";
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+            DeckModel deck = JsonConvert.DeserializeObject<DeckModel>(JSON);
+            return deck;
+        }
+
+
+    }
+}
